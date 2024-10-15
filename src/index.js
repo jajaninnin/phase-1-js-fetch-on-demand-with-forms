3d3ec8c779578ed79adc06d0aaedef148303ab1e
janine pamintuan <jaja.ninnin@gmail.com>
2024-10-15 19:27:26 -0400

    js fetch on demand

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
 const init = () => {
-  
+    const inputForm = document.querySelector("form");
+    inputForm.addEventListener("submit", (event) => {
+        event.preventDefault();
+        const input = document.querySelector("#searchByID")
+        console.log(input.value);
+
+        fetch(`http://localhost:3000/movies/${input.value}`)
+        .then((resp) => resp.json())
+        .then((data) => {
+            const title = document.querySelector("#movieDetails h4")
+            const summary = document.querySelector("#movieDetails p")
+            console.log(title)
+            console.log(summary)
+            title.innerText = data.title
+            summary.innerText = data.summary
+        })
+
+    });
 }
 
 document.addEventListener('DOMContentLoaded', init);
